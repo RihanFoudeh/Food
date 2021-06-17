726c5608aad3c0cc9dcc6aeaf02224f0e22df1ea
RihanFoudeh <>
2021-06-17 13:06:22 +0000

    add javascripts

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 alert ("Welcome to my website")
 alert("This site calculates your age")
-var name =prompt("what's your name ? ")
-alert("Welcome "+ name)
+var name =prompt("what's your name ? " )
+
+
+alert("Welcome "+ name )
+
+
+// var answer = prompt('What is your name?','Joe Blogg');
 
 var age =prompt("what's your birthday ? ")
 
@@ -14,26 +19,43 @@ if(age>=18){
 
 var food =prompt("what's your favorite Food (shawarma ,burger ,pizza) ? ")
 
+while (food != "shawarma" && food !="burger" && food != "pizza"){
+  food = prompt("you entered wrong chooses please try agin")
+}
+var counter =prompt("How many items do you want ? "+food)
+ console.log ("the number of iteams = " +counter) ;
 
 if (food == "shawarma"){
+
+
+
+for (var x=0; x<counter ; x++){
+
   document.write("<div>" +"<h1>" + "shawarma " + "</h1>" +
 			 "<img src='https://static.toiimg.com/thumb/64696930.cms?imgsize=329052&width=800&height=800' >"+ "</div>")
-
-      
+ }
 }
 
  else if  (food == "burger"){
+
+for (var x=0; x<counter ; x++){
+
+
   document.write("<div>" +"<h1>" + "burger " + "</h1>" +
 			 "<img src='https://media-cldnry.s-nbcnews.com/image/upload/t_fit-2000w,f_auto,q_auto:best/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p-2870431.jpg' >"+ "</div>")
 
 
-
+}
 
        }
        else if  (food == "pizza"){
+
+
+         for (var x=0; x<counter ; x++){
+
   document.write("<div>" +"<h1>" + "pizza " + "</h1>" +
 			 "<img src='https://www.glutenfreepalate.com/wp-content/uploads/2018/08/Gluten-Free-Pizza-3.2.jpg' >"+ "</div>")
-
+         }
 
 
 
@@ -44,15 +66,31 @@ if (food == "shawarma"){
          var food =prompt("what's your favorite Food (Happy meal ,small burger ) ? ")
 
        }
-       if(food=="Happy meal"){  document.write("<div>" +"<h1>" + "Happy meal " + "</h1>" +
+       if(food=="Happy meal"){ 
+         
+         
+         for (var x=0; x<counter ; x++){
+
+         
+          document.write("<div>" +"<h1>" + "Happy meal " + "</h1>" +
 			 "<img src='https://image.cnbcfm.com/api/v1/image/105032989-GettyImages-139040294.jpg?v=1607105397&w=740&h=416' >"+ "</div>")
+
+         }
 
        }
 
 
         if(food=="small burger")
-        {  document.write("<div>" +"<h1>" + "small burger " + "</h1>" +
+        {  
+          
+          
+          
+          for (var x=0; x<counter ; x++){
+
+          document.write("<div>" +"<h1>" + "small burger " + "</h1>" +
 			 "<img src='https://www.mashed.com/img/gallery/why-mcdonalds-burgers-are-so-delicious/intro-1595863311.jpg' >"+ "</div>")
+       
+          }
        }
 
 alert("Thank you for visiting my website")
